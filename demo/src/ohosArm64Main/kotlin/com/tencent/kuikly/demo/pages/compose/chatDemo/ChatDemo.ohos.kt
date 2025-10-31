b23151f84bf8f3bc6e6e3e004b75ed00c5f96074
lxx <91441717+elixxli@users.noreply.github.com>
2025-10-31 21:42:30 +0800

    chore: revert AI Chat Demo (#619)
    Co-authored-by: elixxli <elixxli@tencent.com>

--- demo/src/ohosArm64Main/kotlin/com/tencent/kuikly/demo/pages/compose/chatDemo/ChatDemo.ohos.kt
+++ demo/src/ohosArm64Main/kotlin/com/tencent/kuikly/demo/pages/compose/chatDemo/ChatDemo.ohos.kt
@@ -1,6 +1,6 @@
-package com.tencent.kuikly.demo.pages.compose.chatDemo
-
-internal actual object NetworkClient {
-    actual val client: Any?
-        get() = null
-}
+//package com.tencent.kuikly.demo.pages.compose.chatDemo
+//
+//internal actual object NetworkClient {
+//    actual val client: Any?
+//        get() = null
+//}
