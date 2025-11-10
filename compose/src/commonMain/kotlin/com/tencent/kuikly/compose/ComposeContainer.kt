7f90af3f20c30ff7509c3694b461cd3dbae8297f
zenipchen <870658410@qq.com>
2025-11-10 14:03:44 +0800

    feat: enableConsumeSnapshotWhenPause flags to resolve conflict with official compose (#645)
    Co-authored-by: zhenhuachen <zhenhuachen@tencent.com>

--- compose/src/commonMain/kotlin/com/tencent/kuikly/compose/ComposeContainer.kt
+++ compose/src/commonMain/kotlin/com/tencent/kuikly/compose/ComposeContainer.kt
@@ -24,6 +24,7 @@ import com.tencent.kuikly.compose.coroutines.internal.ComposeDispatcher
 import com.tencent.kuikly.compose.foundation.event.OnBackPressedDispatcher
 import com.tencent.kuikly.compose.foundation.event.OnBackPressedDispatcherOwner
 import com.tencent.kuikly.compose.ui.ExperimentalComposeUiApi
+import com.tencent.kuikly.compose.ui.GlobalSnapshotManager
 import com.tencent.kuikly.compose.ui.InternalComposeUiApi
 import com.tencent.kuikly.compose.ui.platform.WindowInfoImpl
 import com.tencent.kuikly.compose.ui.scene.ComposeScene
@@ -127,11 +128,13 @@ open class ComposeContainer :
 
     override fun pageDidAppear() {
         super.pageDidAppear()
+        GlobalSnapshotManager.resume()
         mediator?.updateAppState(true)
     }
 
     override fun pageDidDisappear() {
         super.pageDidDisappear()
+        GlobalSnapshotManager.pause()
     }
 
     override fun pageWillDestroy() {
@@ -153,6 +156,7 @@ open class ComposeContainer :
             boundsInWindow = IntRect(0, 0, windowInfo.containerSize.width, windowInfo.containerSize.height),
             invalidate = invalidate,
             coroutineContext = coroutineContext,
+            enableConsumeSnapshotWhenPause = enableConsumeSnapshotWhenPause(),
         )
 
     private fun createMediatorIfNeeded() {
@@ -237,6 +241,16 @@ open class ComposeContainer :
 
     override fun isAccessibilityRunning(): Boolean {
         return pageData.isAccessibilityRunning
+    }
+
+    /**
+     * 当KuiklyCompose和原生Compose同时存在时候，通过覆盖该方法禁止KuiklyCompose页面在后台时
+     * 会继续消费Compose Runtime 共享 Snapshot的变更。解决原生Compose的重组状态偶现丢失的问题。
+     *
+     * 如果业务仅仅在Android平台使用了原生Compose，可以单独在Android平台返回false来规避问题
+     */
+    open fun enableConsumeSnapshotWhenPause(): Boolean {
+        return true
     }
 
     /**
